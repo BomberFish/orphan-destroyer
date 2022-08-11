acf956745e6da6773fef32a2349e34ad8e052712
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-08-11 14:16:52 -0400

    initial

---
+++ index.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, Routes, Client, MessageAttachment, GatewayIntentBits } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { clientId, guildId, token } = require('./auth.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+  client.user.setPresence({
+    game: {
+     name: 'with the discord.js library',
+     type: 'PLAYING',
+    },
+    status: 'online',
+   });
+});
+
+// Login to Discord with your client's token
+client.login(token);
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('pong'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nCreated on: ${interaction.guild.createdAt}\nID: ${interaction.guild.id}`);
+	} 
+});
